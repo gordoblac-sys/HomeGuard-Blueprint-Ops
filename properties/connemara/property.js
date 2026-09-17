5872d981cb809a51d9635ddaa10615a5f631406c
gordoblac-sys <gordo.blac@gmail.com>
2026-09-17 12:53:39 -0400

    Add real Connemara property mission package

---
+++ properties/connemara/property.js
@@ -0,0 +1,65 @@
+// Real-property mission package: Carl Sandburg Home / Connemara
+// All panorama URLs below depict the same National Park Service property.
+// NPS-produced images are public-domain U.S. federal government works.
+
+window.HOMEGUARD_PROPERTIES = window.HOMEGUARD_PROPERTIES || {};
+
+window.HOMEGUARD_PROPERTIES.connemara = {
+  id: 'connemara',
+  name: 'Connemara Main House',
+  subtitle: 'Carl Sandburg Home National Historic Site',
+  location: 'Flat Rock, North Carolina',
+  propertyType: 'Historic detached residence',
+  sourceType: 'Real property / public-domain prototype',
+
+  floorPlans: {
+    sourceReport: 'Connemara Main House Historic Structure Report',
+    sourceUrl: 'https://npshistory.com/publications/carl/hsr-connemara-main-house.pdf',
+    groundFloorReportPage: 171,
+    firstFloorReportPage: 173,
+    secondFloorReportPage: 175,
+    note: 'Use the official 2004 measured plans as the geometry reference for blueprint overlays and navigation.'
+  },
+
+  virtualTour: {
+    sourceUrl: 'https://www.nps.gov/carl/planyourvisit/virtual-tour.htm',
+    note: 'NPS virtual tour and NPS-authored 360 imagery from the same home.'
+  },
+
+  panoramas: {
+    'Front Porch': {
+      file: '360 degree view from the Sandburg Home porch. (db7fe087-bc69-42c1-9cd3-02aea4c0f7d6).jpg',
+      commonsPage: 'https://commons.wikimedia.org/wiki/File:360_degree_view_from_the_Sandburg_Home_porch._(db7fe087-bc69-42c1-9cd3-02aea4c0f7d6).jpg'
+    },
+    'Living Room': {
+      file: 'Sandburg Home Living Room (b7a0ca83-a5e6-41b5-bc4b-bd2f97ae97a2).jpg',
+      commonsPage: 'https://commons.wikimedia.org/wiki/File:Sandburg_Home_Living_Room_(b7a0ca83-a5e6-41b5-bc4b-bd2f97ae97a2).jpg'
+    },
+    'Kitchen': {
+      file: '360 view of the Sandburg Home Kitchen (d0da8053-8da5-4c70-8e49-281c084900ec).jpg',
+      commonsPage: 'https://commons.wikimedia.org/wiki/File:360_view_of_the_Sandburg_Home_Kitchen_(d0da8053-8da5-4c70-8e49-281c084900ec).jpg'
+    },
+    "Mrs. Sandburg's Bedroom": {
+      file: "360 view of Mrs. Sandburg's Bedroom (2a47eada-2d4c-4c54-8bce-8fe81c56e617).jpg",
+      commonsPage: "https://commons.wikimedia.org/wiki/File:360_view_of_Mrs._Sandburg%27s_Bedroom_(2a47eada-2d4c-4c54-8bce-8fe81c56e617).jpg"
+    },
+    'Guest Room': {
+      file: 'Sandburg Home Guest Room (7c0014e7-ccbc-40dc-be23-25236f546c2f).jpg',
+      commonsPage: 'https://commons.wikimedia.org/wiki/File:Sandburg_Home_Guest_Room_(7c0014e7-ccbc-40dc-be23-25236f546c2f).jpg'
+    },
+    "Mrs. Sandburg's Farm Office": {
+      file: "Mrs. Sandburg's Farm Office (add7861e-f5d7-4fc1-adef-0919ea7f5b2f).jpg",
+      commonsPage: "https://commons.wikimedia.org/wiki/File:Mrs._Sandburg%27s_Farm_Office_(add7861e-f5d7-4fc1-adef-0919ea7f5b2f).jpg"
+    },
+    "Carl Sandburg's Correspondence Office": {
+      file: "Carl Sandburg's Correspondence Office (534e4d1b-041b-414c-947c-8479a7975bb8).jpg",
+      commonsPage: "https://commons.wikimedia.org/wiki/File:Carl_Sandburg%27s_Correspondence_Office_(534e4d1b-041b-414c-947c-8479a7975bb8).jpg"
+    }
+  },
+
+  license: {
+    panoramaStatus: 'Public domain in the United States',
+    panoramaReason: 'National Park Service employee work created as part of official duties',
+    floorPlanStatus: 'U.S. government historic-structure documentation; use official source and attribution in project notes'
+  }
+};
